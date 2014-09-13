86f815929612913896847b0480d26554bd7d222e
sotirisnik <sotirisnik@gmail.com>
2014-09-14 02:48:32 +0300

    problem 694

--- 694.cpp
+++ 694.cpp
@@ -2,35 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <map>
 #include <algorithm>
 
 using namespace std;
 
 int T, A, L;
 
-map <long long,long long> memo;
-
 long long f( long long n ) {
 
     if ( n == 1 ) {
         return 1;
     }
-
-    if ( memo.find( n ) != memo.end() ) {
-        return ( memo[n] );
-    }
     
     if ( n & 1 ) {
     
         if ( 3*n + 1 > L ) {
-            return ( memo[n] = 1 );
+            return ( 1 );
         }
     
-        return ( memo[ n ] = 1 + f( 3*n + 1 ) );
+        return ( 1 + f( 3*n + 1 ) );
     }
     
-    return ( memo[n] = 1 + f( n / 2 ) );
+    return ( 1 + f( n / 2 ) );
 
 }
 
@@ -44,8 +37,6 @@ int main( ) {
             break;
         }
         
-        memo.clear();
-    
         printf( "Case %d: A = %d, limit = %d, number of terms = %lld\n", T+1, A, L, f(A) );
         
     }
